b485b7dc0718ca50e8b5bac5ce2263a3ca43df22
hyjoong <maplesosser@naver.com>
2021-07-13 10:57:51 +0900

    initial setting

---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import { Main } from "Main";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GlobalStyle from "styles/GlobalStyle";
+
+const Routes: React.FunctionComponent = () => {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  black: "#333333",
+  white: "#FFFFFF",
+  grey: "#999999",
+  lightGrey: "rgba(0,0,0,0.1)",
+  blue: "#258BF7",
+} as const;
